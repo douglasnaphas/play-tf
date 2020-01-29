17d89d05aa0ee3359d94d218c50f4e53c6d6aac4
Douglas Naphas <douglasnaphas@gmail.com>
2020-01-28 20:19:21 -0500

    Add dependent resource
    https://learn.hashicorp.com/terraform/getting-started/dependencies

--- example.tf
+++ example.tf
@@ -3,7 +3,12 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_instance" "example1" {
+resource "aws_instance" "example" {
   ami           = "ami-b374d5a5"
   instance_type = "t2.micro"
+}
+
+resource "aws_eip" "ip" {
+  vpc      = true
+  instance = "${aws_instance.example.id}"
 }
